3fc0cc626b1bfa99bed0e89221ec32fbca456afb
Robbie Greiner <robbieg@gmail.com>
2017-12-21 14:45:51 -0700

    styling of controls

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,14 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
 import '../styles/App.css';
-import Main from './Main.js';
+import Controls from './Controls.js';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Route path="/" component={ Main } />
-        {/* <Route path="/" component= { Controls } />
-        <Route exact path="/" component= { DataViz } /> */}
+        <Route path="/" component= { Controls } />
+        {/* <Route exact path="/" component= { DataViz } /> */}
         {/* <Route exact path="/employees" component= { employeesContainer } />
         <Route exact path="/employees/:id" component= { employeesContainer } /> */}
         {/* <Route path="/employees" component={ employees } /> */}
---
+++ src/components/Controls.js
@@ -0,0 +1,65 @@
+import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
+import '../styles/Controls.css';
+
+class Controls extends Component {
+  render() {
+    return (
+      <section className="main-container">
+        <div className="controls">
+          <h1>Controls</h1>
+          <hr/>
+          <div className="button-container">
+            <div className="button">
+              <div className="circle-icon"></div>
+              <p className="button-name">Projects</p>
+            </div>
+
+            <div className="button">
+              <div className="circle-icon"></div>
+              <p className="button-name">Projects</p>
+            </div>
+
+            <div className="button">
+              <div className="circle-icon"></div>
+              <p className="button-name">Projects</p>
+            </div>
+
+            <div className="button">
+              <div className="circle-icon"></div>
+              <p className="button-name">Projects</p>
+            </div>
+
+            <div className="button">
+              <div className="circle-icon"></div>
+              <p className="button-name">Projects</p>
+            </div>
+
+            <div className="button">
+              <div className="circle-icon"></div>
+              <p className="button-name">Projects</p>
+            </div>
+
+            <div className="button">
+              <div className="circle-icon"></div>
+              <p className="button-name">Projects</p>
+            </div>
+
+            <div className="button">
+              <div className="circle-icon"></div>
+              <p className="button-name">Projects</p>
+            </div>
+
+            <div className="button">
+              <div className="circle-icon"></div>
+              <p className="button-name">Projects</p>
+            </div>
+
+          </div>
+        </div>
+      </section>
+    );
+  }
+}
+
+export default Controls;
