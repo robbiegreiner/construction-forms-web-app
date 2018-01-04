cd8c2b3d28f50fd5b486120144e484d896737dda
Nick Teets <nickteets@gmail.com>
2018-01-04 13:09:26 -0700

    Post new employees from web app

--- src/components/Home.js
+++ src/components/Home.js
@@ -11,6 +11,7 @@ class Home extends Component {
 
     this.state = {
       selectedControl: '',
+      newEmployee: {}
     };
   }
 
@@ -18,12 +19,92 @@ class Home extends Component {
     this.setState({selectedControl: control});
   }
 
+  handleEmployeeChange = (event) => {
+    const newK = event.target.className
+    const newO = Object.assign({}, this.state.newEmployee, { [event.target.className]: event.target.value })
+    this.setState({
+      newEmployee: newO
+    })
+  }
+
+  postNewEmployee = (event) => {
+    event.preventDefault();
+    const postBody = Object.assign({},
+      {
+        name: this.state.newEmployee.employee_name,
+        position: this.state.newEmployee.employee_position,
+        email: this.state.newEmployee.employee_email,
+        phone: this.state.newEmployee.employee_phone,
+      }
+    );
+    fetch(`http://localhost:4000/api/v1/employees`, {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify(postBody)
+    })
+      .then(res => res.json())
+      .catch(error => { throw error; });
+  }
+
+
   renderPopUp() {
     if (this.state.selectedControl === 'Add Employee') {
       return (
-        <div>hey</div>
+        <div className="add-popup">
+          <form className="popup-form" onSubmit={this.postNewEmployee}>
+            <button 
+              onClick={() => this.setState({ selectedControl: '' })} 
+              className="close-popup"
+            >X</button>
+            <h4>Add New Employee</h4>
+            <label htmlFor="employee-name">Name: 
+              <input 
+                type="text" 
+                ref="employee-name" 
+                className="employee_name"  
+                placeholder="Name" 
+                onChange={this.handleEmployeeChange}
+              />
+            </label>
+            <label htmlFor="employee-position">Position: 
+              <input 
+                type="text" 
+                ref="employee-position" 
+                className="employee_position"  
+                placeholder="Position" 
+                onChange={this.handleEmployeeChange}
+              />
+            </label>
+            <label htmlFor="employee-email">Email: 
+              <input 
+                type="email" 
+                ref="employee-email" 
+                className="employee_email"  
+                placeholder="Email" 
+                onChange={this.handleEmployeeChange}
+              />
+            </label>
+            <label htmlFor="employee-phone">Phone: 
+              <input 
+                type="text" 
+                ref="employee-phone" 
+                className="employee_phone"  
+                placeholder="Phone" 
+                onChange={this.handleEmployeeChange}
+              />
+            </label>
+            <input type="submit" value="Submit" />
+          </form>
+        </div>
       )
     }
+  }
+
+  postNewEmployee(event) {
+    event.preventDefault();
+    console.log(event.target) 
   }
 
   render() {
