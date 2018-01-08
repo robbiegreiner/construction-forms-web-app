3a7fb2c839f3649cd50913899b0d6da0bd2cdecb
Nick Teets <nickteets@gmail.com>
2018-01-07 21:30:06 -0700

    Create switch statement in dataviz

--- src/components/DataViz.js
+++ src/components/DataViz.js
@@ -107,182 +107,187 @@ class DataViz extends Component {
     );
   }
 
+  renderWelcome = (selectedControl) => {
+    if (!selectedControl) {
+      return (
+        <div className="welcome">
+          <h1>Welcome to Construction Forms!</h1>
+          <div className="logo"></div>
+        </div>
+      )
+    }
+  }
+
+  cardSwitch = (selectedControl) => {
+    switch (selectedControl) {
+    case 'Projects':
+      return this.state.projects.map((project, index) => {
+        return <Card
+          key={'project' + index}
+          header={project.name}
+          body={project.location}
+          projectInfo={[project.public, project.union]}
+          cardClickHandler={this.cardClickHandler}
+        />;
+      });
+    case 'Employees':
+      return this.state.employees.map((employee, index) => {
+        return <Card
+          key={'employee' + index}
+          header={employee.name}
+          body={employee.position}
+          employeeInfo={[employee.phone, employee.email]}
+          cardClickHandler={this.cardClickHandler}
+        />;
+      });
+    case 'All Reports':
+      return this.state.hotworkForms.concat(this.state.pretaskForms).map((form, index) => {
+        const sigImg = new Image();
+        sigImg.src = form.signature;
+        return <Card
+          key={'form' + index}
+          header={
+            Object.keys(form).includes('firewatchRequirement') ? 'Hotwork Permit - ' + form.employee_name : 'Pretask Permit - ' + form.employee_name
+          }
+          cardClickHandler={this.cardClickHandler}
+          formDetails={[
+            form.employee_email,
+            form.project_id,
+            form.company,
+            form.date,
+            form.firewatchRequirement,
+            form.areaInspected,
+            form.fireExtinguisher,
+            form.flammablesRemoved,
+            form.sprinklerHeadsProtected,
+            form.signature,
+            sigImg
+          ]}
+        />;
+      });
+    case 'All Forms':
+      return (
+        <div className="all-forms">
+          <HotworkForm />
+          <PretaskForm />
+        </div>
+      );
+    case 'Reports By Project':
+      return (
+        <div>
+          <h2 className="data-viz-cards-header">Select a project</h2>
+          <div className="data-viz-cards">
+            {
+              this.state.dataVizControl &&
+                <div className="selected-div">
+                  {
+                    this.state.hotworkForms.concat(this.state.pretaskForms).map((form, index) => {
+                      if (parseInt(this.state.dataVizControl.split(' - ')[1]) === form.project_id) {
+                        const sigImg = new Image();
+                        sigImg.src = form.signature;
+                        return <Card
+                          key={'form' + index}
+                          header={
+                            Object.keys(form).includes('firewatchRequirement') ? 'Hotwork Permit - ' + form.employee_name : 'Pretask Permit - ' + form.employee_name
+                          }
+                          cardClickHandler={this.mockCardClickHandler}
+                          formDetails={[
+                            form.employee_email,
+                            form.project_id,
+                            form.company,
+                            form.date,
+                            form.firewatchRequirement,
+                            form.areaInspected,
+                            form.fireExtinguisher,
+                            form.flammablesRemoved,
+                            form.sprinklerHeadsProtected,
+                            form.signature,
+                            sigImg
+                          ]}
+                        />;
+                      }
+                    })
+                  }
+                </div>
+            }
+            {
+              this.state.projects.map((project, index) => {
+                return <Card
+                  header={project.name}
+                  body={<button onClick={() => this.reportsBySelectionHandler('Reports By Project - ' + project.id)}>Select</button>}
+                  projectId={project.id}
+                  key={'project' + index}
+                  cardClickHandler={this.cardClickHandler}
+                  reportsBySelectionHandler={this.reportsBySelectionHandler}
+                />;
+              })
+            }
+          </div>
+        </div>
+      );
+    case 'Reports By Employee':
+      return (
+        <div>
+          <h2 className="data-viz-cards-header">Select an employee</h2>
+          <div className="data-viz-cards">
+            {
+              this.state.dataVizControl &&
+                <div className="selected-div">
+                  {
+                    this.state.hotworkForms.concat(this.state.pretaskForms).map((form, index) => {
+                      if (this.state.dataVizControl.split(' - ')[1] === form.employee_id) {
+                        const sigImg = new Image();
+                        sigImg.src = form.signature;
+                        return <Card
+                          key={'form' + index}
+                          header={
+                            Object.keys(form).includes('firewatchRequirement') ? 'Hotwork Permit - ' + form.employee_name : 'Pretask Permit - ' + form.employee_name
+                          }
+                          cardClickHandler={this.mockCardClickHandler}
+                          formDetails={[
+                            form.employee_email,
+                            form.project_id,
+                            form.company,
+                            form.date,
+                            form.firewatchRequirement,
+                            form.areaInspected,
+                            form.fireExtinguisher,
+                            form.flammablesRemoved,
+                            form.sprinklerHeadsProtected,
+                            form.signature,
+                            sigImg
+                          ]}
+                        />;
+                      }
+                    })
+                  }
+                </div>
+            }
+            {
+              this.state.employees.map((employee, index) => {
+                return <Card
+                  header={employee.name}
+                  body={<button onClick={() => this.reportsBySelectionHandler('Reports By Employee - ' + employee.id)}>Select</button>}
+                  employeeId={employee.id}
+                  key={'employee' + index}
+                  reportsBySelectionHandler={this.reportsBySelectionHandler}
+                  cardClickHandler={this.cardClickHandler}
+                />;
+              })
+            }
+          </div>
+        </div>
+      );
+    }
+  }
+
   render() {
     return (
       <div className="data-viz">
-        {
-          !this.props.selectedControl &&
-            <div className="welcome">
-              <h1>Welcome to Construction Forms!</h1>
-              <div className="logo"></div>
-            </div>
-        }
+        { this.renderWelcome(this.props.selectedControl) }
         { this.renderSelected(this.state.selectedInfo) }
         <div className="data-viz-cards">
-          {
-            this.props.selectedControl === 'Projects' &&
-              this.state.projects.map((project, index) => {
-                return <Card
-                  key={'project' + index}
-                  header={project.name}
-                  body={project.location}
-                  projectInfo={[project.public, project.union]}
-                  cardClickHandler={this.cardClickHandler}
-                />;
-              })
-          }
-          {
-            this.props.selectedControl === 'Employees' &&
-              this.state.employees.map((employee, index) => {
-                return <Card
-                  key={'employee' + index}
-                  header={employee.name}
-                  body={employee.position}
-                  employeeInfo={[employee.phone, employee.email]}
-                  cardClickHandler={this.cardClickHandler}
-                />;
-              })
-          }
-          {
-            this.props.selectedControl === 'All Reports' &&
-              this.state.hotworkForms.concat(this.state.pretaskForms).map((form, index) => {
-                const sigImg = new Image();
-                sigImg.src = form.signature;
-                return <Card
-                  key={'form' + index}
-                  header={
-                    Object.keys(form).includes('firewatchRequirement') ? 'Hotwork Permit - ' + form.employee_name : 'Pretask Permit - ' + form.employee_name
-                  }
-                  cardClickHandler={this.cardClickHandler}
-                  formDetails={[
-                    form.employee_email,
-                    form.project_id,
-                    form.company,
-                    form.date,
-                    form.firewatchRequirement,
-                    form.areaInspected,
-                    form.fireExtinguisher,
-                    form.flammablesRemoved,
-                    form.sprinklerHeadsProtected,
-                    form.signature,
-                    sigImg
-                  ]}
-                />;
-              })
-          }
-          {
-            this.props.selectedControl === 'Reports By Project' &&
-              <div>
-                <h2 className="data-viz-cards-header">Select a project</h2>
-                <div className="data-viz-cards">
-                  {
-                    this.state.dataVizControl &&
-                      <div className="selected-div">
-                        {
-                          this.state.hotworkForms.concat(this.state.pretaskForms).map((form, index) => {
-                            if (parseInt(this.state.dataVizControl.split(' - ')[1]) === form.project_id) {
-                              const sigImg = new Image();
-                              sigImg.src = form.signature;
-                              return <Card
-                                key={'form' + index}
-                                header={
-                                  Object.keys(form).includes('firewatchRequirement') ? 'Hotwork Permit - ' + form.employee_name : 'Pretask Permit - ' + form.employee_name
-                                }
-                                cardClickHandler={this.mockCardClickHandler}
-                                formDetails={[
-                                  form.employee_email,
-                                  form.project_id,
-                                  form.company,
-                                  form.date,
-                                  form.firewatchRequirement,
-                                  form.areaInspected,
-                                  form.fireExtinguisher,
-                                  form.flammablesRemoved,
-                                  form.sprinklerHeadsProtected,
-                                  form.signature,
-                                  sigImg
-                                ]}
-                              />;
-                            }
-                          })
-                        }
-                      </div>
-                  }
-                  {
-                    this.state.projects.map((project, index) => {
-                      return <Card
-                        header={project.name}
-                        body={<button onClick={() => this.reportsBySelectionHandler('Reports By Project - ' + project.id)}>Select</button>}
-                        projectId={project.id}
-                        key={'project' + index}
-                        cardClickHandler={this.cardClickHandler}
-                        reportsBySelectionHandler={this.reportsBySelectionHandler}
-                      />;
-                    })
-                  }
-                </div>
-              </div>
-          }
-          {
-            this.props.selectedControl === 'Reports By Employee' &&
-              <div>
-                <h2 className="data-viz-cards-header">Select an employee</h2>
-                <div className="data-viz-cards">
-                  {
-                    this.state.dataVizControl &&
-                      <div className="selected-div">
-                        {
-                          this.state.hotworkForms.concat(this.state.pretaskForms).map((form, index) => {
-                            if (this.state.dataVizControl.split(' - ')[1] === form.employee_id) {
-                              const sigImg = new Image();
-                              sigImg.src = form.signature;
-                              return <Card
-                                key={'form' + index}
-                                header={
-                                  Object.keys(form).includes('firewatchRequirement') ? 'Hotwork Permit - ' + form.employee_name : 'Pretask Permit - ' + form.employee_name
-                                }
-                                cardClickHandler={this.mockCardClickHandler}
-                                formDetails={[
-                                  form.employee_email,
-                                  form.project_id,
-                                  form.company,
-                                  form.date,
-                                  form.firewatchRequirement,
-                                  form.areaInspected,
-                                  form.fireExtinguisher,
-                                  form.flammablesRemoved,
-                                  form.sprinklerHeadsProtected,
-                                  form.signature,
-                                  sigImg
-                                ]}
-                              />;
-                            }
-                          })
-                        }
-                      </div>
-                  }
-                  {
-                    this.state.employees.map((employee, index) => {
-                      return <Card
-                        header={employee.name}
-                        body={<button onClick={() => this.reportsBySelectionHandler('Reports By Employee - ' + employee.id)}>Select</button>}
-                        employeeId={employee.id}
-                        key={'employee' + index}
-                        reportsBySelectionHandler={this.reportsBySelectionHandler}
-                        cardClickHandler={this.cardClickHandler}
-                      />;
-                    })
-                  }
-                </div>
-              </div>
-          }
-          {
-            this.props.selectedControl === 'All Forms' &&
-              <div className="all-forms">
-                <HotworkForm />
-                <PretaskForm />
-              </div>
-          }
+          { this.cardSwitch(this.props.selectedControl) }
         </div>
       </div>
 
