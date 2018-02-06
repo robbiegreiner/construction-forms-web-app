d7bbaad6b9c34d9e5d1b18d1181ad8192929364b
Nick Teets <nickteets@gmail.com>
2018-02-05 17:35:56 -0700

    About page is completed

--- src/components/About.js
+++ src/components/About.js
@@ -4,10 +4,12 @@ import '../styles/About.css';
 
 const About = () => ( 
   <div class="about-div">
-    <h3>Construction Forms</h3>
+    <h2>Construction Forms</h2>
     <div class="hero"></div>
     <p>Construction Forms aims to eliminate the inefficiencies of construction documentation at the field level.  This includes a native mobile app that allows tradespeople to fill out their usual planning and safety forms on their iPhone or Android device.  
     Most importantly, the app includes a web portal for administrators to gather all the form data and sort it by project or employee.  This eliminates paper, filing time and data entry that would usually fall on the shoulders of a project admin or engineer.</p>
+    <br />
+    <a href="https://github.com/robbiegreiner/construction-forms-web-app" target="_blank">Find out more on GitHub</a>
   </div>
 );
 
